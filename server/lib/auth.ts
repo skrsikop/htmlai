28586eccaf438101fe7b6bf8eaa9d00af6225779
skrsikop <skrsikop@gmail.com>
2026-01-07 18:57:33 +0300

    final code

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -3,7 +3,6 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma.js";
 
-const trustedOrigins = process.env.TRUSTED_ORIGINS?.split(",") || [];
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -15,7 +14,6 @@ export const auth = betterAuth({
   user: {
     deleteUser: {enabled: true}
   },
-  trustedOrigins,
   baseURL: process.env.BETTER_AUTH_URL!,
   secret: process.env.BETTER_AUTH_SECRET!,
   advanced: {
